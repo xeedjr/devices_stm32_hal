4eb5bacda606902370a3558b301c2dd4a9b462a7
Bogdan <xeedjr@gmail.com>
2020-10-13 11:44:02 +0300

    Update

--- RPMEncoderOptical.cpp
+++ RPMEncoderOptical.cpp
@@ -7,10 +7,13 @@
 
 #include <RPMEncoderOptical.h>
 
-RPMEncoderOptical::RPMEncoderOptical(TIM_HandleTypeDef *timer, uint32_t channelRaise, uint32_t channelFall) {
+#define RPM_ENCODER_OPTICAL_GET_PRESCALER(__HANDLE__)       ((__HANDLE__)->Init->Prescaler)
+
+RPMEncoderOptical::RPMEncoderOptical(TIM_HandleTypeDef *timer, uint32_t channelRaise, uint32_t channelFall, uint32_t tick_per_revolution) {
 	this->timer = timer;
 	this->channelRaise = channelRaise;
 	this->channelFall = channelFall;
+	this->tick_per_revolution = tick_per_revolution;
 
 	HAL_TIM_IC_Start(timer, channelRaise);
 	HAL_TIM_IC_Start(timer, channelFall);
@@ -21,12 +24,26 @@ RPMEncoderOptical::~RPMEncoderOptical() {
 }
 
 float RPMEncoderOptical::get_time() {
+	uint32_t current_count = 0;
+
 	uint32_t raise = __HAL_TIM_GET_COMPARE(timer, channelRaise);
 	uint32_t fall = __HAL_TIM_GET_COMPARE(timer, channelFall);
 
-	if (raise < fall) {
-		current_rpm = fall - raise;
+	if (last_fall_count != fall) {
+		if (raise < fall) {
+			current_count = fall - raise;
+		} else {
+			/// we have overflow
+			auto t = __HAL_TIM_GET_AUTORELOAD(timer) - raise;
+			current_count = fall + t;
+		}
+	} else {
+		/// we do not have yet readings
+		current_count = last_count;
 	}
 
-	return current_rpm;
+	last_fall_count = fall;
+	last_count = current_count;
+
+	return ((float)current_count/(float)tick_per_revolution);
 }
--- RPMEncoderOptical.h
+++ RPMEncoderOptical.h
@@ -9,14 +9,20 @@
 
 #include "stm32common_hal.h"
 
+/**
+ * 	auto clock = HAL_RCC_GetPCLK1Freq()/htim2.Init.Prescaler;
+ *	auto pulses = 0.04/(1.0/(float)clock);
+ */
 
 class RPMEncoderOptical {
 	TIM_HandleTypeDef *timer;
 	uint32_t channelRaise, channelFall;
-	float current_rpm = 0;
+	uint32_t tick_per_revolution = 0;
+	uint32_t last_count = 0;
+	float last_fall_count = 0;
 
 public:
-	RPMEncoderOptical(TIM_HandleTypeDef *timer, uint32_t channelRaise, uint32_t channelFall);
+	RPMEncoderOptical(TIM_HandleTypeDef *timer, uint32_t channelRaise, uint32_t channelFall, uint32_t tick_per_revolution);
 	virtual ~RPMEncoderOptical();
 	float get_time();
 };
